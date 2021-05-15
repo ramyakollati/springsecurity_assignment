1e8ff6502374035f7756fd409185041d6ce58eca
ramyakollati <84019206+ramyakollati@users.noreply.github.com>
2021-05-15 11:56:47 +0530

    commit

---
+++ springsecurity2_1/src/main/java/one/SecurityApp.java
@@ -0,0 +1,13 @@
+package one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApp.class, args);
+	}
+
+}
